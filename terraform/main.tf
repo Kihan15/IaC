0e3683d68c24795da640fa8fb1565aa1691d10e8
Kihan15 <test.bin75@gmail.com>
2025-10-04 00:53:26 +0200

    change storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account" "main" {
   resource_group_name       = azurerm_resource_group.main.name
   location                  = azurerm_resource_group.main.location
   account_tier              = "Standard"
-  account_replication_type  = "GRS"
+  account_replication_type  = "LRS"
   min_tls_version           = "TLS1_2"
 
   tags = merge(var.tags, { resource = "storage-account" })

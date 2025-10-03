7f25afed9b627afe8931bd598f2869feee3de0d0
Kihan15 <test.bin75@gmail.com>
2025-10-03 22:29:36 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ provider "azurerm" {
 provider "azurerm" {
   features {}
   alias          = "target_sub"
-  use_oidtc      = true
+  use_oidc      = true
   subscription_id = "d2c5b5b1-d8df-4dbd-ac14-d347e7ab31b0"
 }
 
